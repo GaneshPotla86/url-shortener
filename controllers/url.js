9da0ffc0c4a5445a37969a04bd6990bb81b8b4c9
GaneshPotla86 <potlaganesh563@gmail.com>
2026-03-21 21:26:14 +0530

    mongodb connected and rouets and controllers code is added

---
+++ controllers/url.js
@@ -0,0 +1,14 @@
+const {shortId}=require("shortid")
+const URL=require('../models/url')
+async function handlegenerateNewShortURL(req,res){
+    const body=req.body
+    if(!body.url) return res.status(400).json({error: "url is required"})
+    const shortId=shortId()
+    await URL.create({
+        shortId: shortId,
+        redirectURL: body.url,
+        vistedHistory:[],
+    })
+    return res.json({id:shortId})
+}
+module.exports={handlegenerateNewShortURL,}
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express =require('express')
 const app=express()
+const urlRoute=require('./routes/url')
 const port=8001
-
+//connecting MongoDB
+const {connectDB}=require('./connection')
+connectDB("mongodb://127.0.0.1:27017/url-shortener").then(()=>{
+    console.log("Connected to MongoDB")
+})
+app.use(express.json())
+app.use('/url', urlRoute)
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 })
